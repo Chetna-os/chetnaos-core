88b2e253664703773be5087d6518d1ff0e39c2d3
Mangla Prasad Pandey <manglaprasadpandey@chetna.live>
2025-12-11 22:51:12 +0530

    Implement MotionSimulator using VirtualActuator
    Added MotionSimulator class that utilizes VirtualActuator.

--- backend/trinetra/robotics/motion_simulator.py
+++ backend/trinetra/robotics/motion_simulator.py
@@ -1,1 +1,13 @@
+# backend/trinetra/robotics/motion_simulator.py
+from .virtual_actuator import VirtualActuator
 
+class MotionSimulator:
+    """
+    High-level motion simulator that uses VirtualActuator.
+    """
+    def _init_(self):
+        self.actuator = VirtualActuator()
+
+    def execute(self, command: dict):
+        # command validator could be extended
+        return self.actuator.apply(command)
